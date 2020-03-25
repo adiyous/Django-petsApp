7e00ce61f089d2030fd671aa4bb7f881aaca9afa
Adil <adilnewyork@gmail.com>
2020-03-25 14:33:46 -0300

    Adding Pet and Vaccine class

--- adoptions/models.py
+++ adoptions/models.py
@@ -4,5 +4,16 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Pet(models.Model):
+    SEX_CHOISES = [('M', 'Male'), ('F', 'Female')]
+    name = models.CharField(max_length =100)
+    submitter = models.CharField(max_length =100)
+    species = models.CharField(max_length =100)
+    breed = models.CharField(max_length =30, blank=True)
+    description = models.TextField()
+    sex = models.CharField(choises=SEX_CHOISES, max_length=1, blank=True)
+    submission_date = models.DateTimeField()
+    age = models.IntegerField(null=True)
+    vaccinations = models.ManyToManyField(null=True)
 
 
