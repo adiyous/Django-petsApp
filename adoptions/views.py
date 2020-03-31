2aa516a9d776d795ad94811e6244ef295b6a39d0
Adil <adilnewyork@gmail.com>
2020-03-31 13:16:06 -0300

    Adding httpresponse to views

--- adoptions/views.py
+++ adoptions/views.py
@@ -2,5 +2,15 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home(request):
+    return HttpResponse(
+    '<p>home view<p>'
+    )
+def pet_detail(request, id):
+    return HttpResponse(
+        '<p>pet_detail view with the id {}</p>'.format(id)
+    )
